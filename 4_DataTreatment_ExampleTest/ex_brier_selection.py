2551f787c6a6738b62e16da74d3b05d45f2f4daa
pauline <pauline.turk1997@outlook.fr>
2022-07-27 14:35:24 +0200

    SELECTION EX BRIER PROGRES

--- 4_DataTreatment_ExampleTest/ex_brier_selection.py
+++ 4_DataTreatment_ExampleTest/ex_brier_selection.py
@@ -1,13 +1,13 @@
-# IMPORTS
+"""SELECTION EX FOR BRIER EXPERIMENTS
+nohup python3 ex_brier_selection.py > selection_ex_brier.out 2>&1 &
+"""
 
-import os
-import os.path
-import argparse
+# IMPORTS
 import time
-import pandas as pd
 import csv
 import numpy as np
 import random
+import math
 
 import sys
 from pathlib import Path
@@ -16,41 +16,75 @@ sys.path.append(file.parents[0])
 
 import utils.folder as folder
 
-DATA_GENERAL = ""
-DATA = ""
-FRAC_SEED = ""
-
-N_EX = 10_000_000
+DATA = f"{file.parents[2]}/MNHN_RESULT/2_5_EXAMPLES/EXAMPLES_6_40_50"
+PATH_DICT_FRAC_SEED =  f"{file.parents[2]}/MNHN_RESULT/2_5_EXAMPLES/frac_ex.csv"
 
 
+start_dico = time.time()
+# convert to a dico (do it once for all?)
+dict_fraction = {}
+with open(PATH_DICT_FRAC_SEED, newline='') as csv_frac:
+    reader = csv.DictReader(csv_frac)
+    for row in reader:
+        dict_fraction[row["id_seed"]] = row["frac_ex"]
+end_dico = time.time()
+print("DICO")
+print(f"DONE IN: {round(end_dico - start_dico, 2)} s")
+
+
+n_files = len(dict_fraction)
+
+N_EX_TOTAL = 1_000
+L = 6
+
+FILE_EXAMPLES = f"{file.parents[2]}/MNHN_RESULT/2_5_EXAMPLES/EX_BRIER_TRAIN.csv"
+
+start = time.time()
 
 files = [x for x in Path(DATA).iterdir()]
 
 
-with open(FRAC_SEED, newline='') as csvfrac:  # vérif que c bien read
+counter = 0
+
+with open(FILE_EXAMPLES, 'w',
+            encoding='UTF8', newline='') as f:
+    writer = csv.writer(f)
+    header_context_ol = [f"aa_ol_{i}" for i in range(1, L+1)]
+    header_context_or = [f"aa_or_{i}" for i in range(1, L+1)]
+    header_context_dl = [f"aa_dl_{i}" for i in range(1, L+1)]
+    header_context_dr = [f"aa_dr_{i}" for i in range(1, L+1)]
+    header_info = ["pid", "name_origin", "name_destination", "aa_origin", "aa_destination"]
+
+    header = header_info + header_context_ol + header_context_or + header_context_dl + header_context_dr
+    writer.writerow(header)
+
 
     for file in files:
         accession_num = folder.get_accession_number(file)
         print(f"accession_num: {accession_num}")
-        csvfrac[]
-
+        fraction = float(dict_fraction[accession_num])
         
+        if fraction != 0:
+            N_EX_SEED_FLOAT = fraction*N_EX_TOTAL
+            decimal_part, integer_part = math.modf(float(N_EX_SEED_FLOAT))
+            proba = random.uniform(0, 1)
+            N_EX_SEED_INT = int(integer_part + int(proba < decimal_part))
 
-        with open(file, newline='') as csvfile:
-            reader = csv.DictReader(csvfile)
+            with open(file, newline='') as csvfile:
+                # print(f"N_EX_SEED_INT: {N_EX_SEED_INT}")
+                reader = csv.reader(csvfile)
+                lines = [tuple(line) for line in reader]
+                # print(f"lines:  {lines}")
+                chosen_rows = random.choices(lines, k=N_EX_SEED_INT)   # header comptée?       FAUX
+                # print(f"chosen rows: {chosen_rows}")
+                #np.random.choice([1, 2, 3], size=10, replace=True)
+                for data in chosen_rows:
+                    writer.writerow(data)
 
-            n_examples = len(reader)
-
-
-            index_examples = random.choices()
-            print(file)
-
-            # counter_row = 0
-            for row in reader:
-                for direction in LIST_DIRECTON:
-                    for position in LIST_ABS_POSITION:
-                        dict_dict_3D[direction, position][row['aa_origin']][row['aa_destination']][row[f'aa_{direction}_{position}']] += 1
-                # counter_row += 1
-                # print(f"num_ex: {counter_row}")
         counter += 1
         print(round(100*counter/n_files, 2))
+
+end = time.time()
+
+print("SELECTION EXAMPLES")
+print(f"DONE IN: {round(end - start, 2)} s")
